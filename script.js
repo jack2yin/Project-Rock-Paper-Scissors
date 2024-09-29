c904da001da119a918bd9c73db6b7b9b6676ec47
Jacky <jackjack.yin@gmail.com>
2024-09-29 02:29:58 -0400

    Wrote the logic to play a single round

--- script.js
+++ script.js
@@ -1,22 +1,76 @@
-let rock = 0;
-let paper = 1;
-let scissor = 2;
 
-function getComputerChoice() {  
-    logic = Math.floor(Math.random() * 3); 
-    if (logic == rock) {
-        console.log("rock")
-    } else if (logic == paper) {
-        console.log("paper")
-    } else if (logic == scissor) {
-        console.log("scissor")
+let humanScore = 0;
+let computerScore = 0;
+
+let getComputerChoice = Math.floor(Math.random() * 3);
+if (getComputerChoice == 0) {
+    //console.log("Computer choice's" + " " + "rock")
+    getComputerChoice = "rock";
+}
+if (getComputerChoice == 1) {
+    //console.log("Computer choice's" + " " + "paper")
+    getComputerChoice = "paper";
+}
+if (getComputerChoice == 2) {
+    //console.log("Computer choice's" + " " + "scissor")
+    getComputerChoice = "scissor";
+}
+
+let getHumanChoice = prompt('Enter rock, paper, or scissor');
+
+let userInputRock = /rock/i;
+let userInputPaper = /paper/i;
+let userInputScissor = /scissor/i;
+
+if (userInputRock.test(getHumanChoice) 
+    || userInputPaper.test(getHumanChoice)
+    || userInputScissor.test(getHumanChoice)) {
+    console.log("You entered" + " " + getHumanChoice.toLowerCase());
+} else if (!userInputRock.test(getHumanChoice) 
+    || !userInputPaper.test(getHumanChoice)
+    || !userInputScissor.test(getHumanChoice)) {
+    alert("Please try again. Must reload the page to play again if an invalid choice was entered. Scores will reset.");
+} else {console.log("You entered" + " " + getHumanChoice.toLowerCase());}
+
+while (!getHumanChoice) {
+    getHumanChoice = prompt('Enter rock, paper, or scissor');
+    if (userInputRock.test(getHumanChoice) 
+        || userInputPaper.test(getHumanChoice)
+        || userInputScissor.test(getHumanChoice)) {
+        console.log("You entered" + " " + getHumanChoice.toLowerCase());
+    } else if (!userInputRock.test(getHumanChoice) 
+        || !userInputPaper.test(getHumanChoice)
+        || !userInputScissor.test(getHumanChoice))
+    {
+        alert("Must reload the page to play again if an invalid choice was entered. Scores will reset.");
     }
- }
+}
 
-let getHumanChoice = prompt("Rock, Paper or Scissor", " ");
-console.log(getHumanChoice);
+const humanSelection = getHumanChoice.toLowerCase();
+const computerSelection = getComputerChoice;
 
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanSelection;
+    computerChoice = computerSelection;
 
+    let TIE = "It's a TIE!";
+    let computerWon = "Computer Won!";
+    let humanWon = "YOU WON";
 
-
- 
+    if (humanChoice == "rock" && computerChoice == "rock"
+        || humanChoice == "paper" && computerChoice == "paper"
+        || humanChoice == "scissor" && computerChoice == "scissor"
+    ) {
+        return TIE;
+    } else if (humanChoice == "rock" && computerChoice == "paper"
+        || humanChoice == "paper" && computerChoice == "scissor"
+        || humanChoice == "scissor" && computerChoice == "rock"
+    ) {
+        return computerWon;
+    } else if (humanChoice == "rock" && computerChoice == "scissor"
+        || humanChoice == "paper" && computerChoice == "rock"
+        || humanChoice == "scissor" && computer == "paper"
+    ) {
+        return humanWon;
+    }
+}
